e377cc1c35d88758f5b46cc13053b423892c6459
vlieonov <lieonov.volodymyr2006@gmail.com>
2025-02-11 11:09:19 +0200

    user info getting test

---
+++ src/main/java/com/vlieonov/projectapi/api/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.vlieonov.projectapi.api.controller;
+
+import com.vlieonov.projectapi.api.model.User;
+import com.vlieonov.projectapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+
+@RestController
+public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public User getUser(@RequestParam Integer id) {
+    Optional<User> user = userService.getUser(id);
+    if(user.isPresent()) {
+    return (User) user.get();
+    }
+    return null;
+    }
+}
+
