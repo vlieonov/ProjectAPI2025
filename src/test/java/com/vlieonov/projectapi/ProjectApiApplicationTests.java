3375408515a71033ba23d60050155fad9b25a400
vlieonov <lieonov.volodymyr2006@gmail.com>
2025-02-11 10:12:24 +0200

    Built project

---
+++ src/test/java/com/vlieonov/projectapi/ProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlieonov.projectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
